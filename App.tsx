c83db8871b28befc377151018dcba714fb21f11d
inespr <iperrom@g.educaand.es>
2023-05-11 09:17:28 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TodoListScreen } from './src/screens/TodoListScreen';
+
+const App = () => {
+  return (
+    <TodoListScreen/>
+  )
+}
+
+export default App;
