7977e1a07ff464ac216ae6e9b7036ff67440fa81
inespr <iperrom@g.educaand.es>
2023-05-15 14:05:25 +0200

    Delete

--- src/interfaces/DaySection.tsx
+++
@@ -1,7 +0,0 @@
-import Task from "./Task";
-
-interface DaySection {
-  title: string;
-  data: Task[];
-}
-export default DaySection;
--- src/interfaces/Task.tsx
+++
@@ -1,9 +0,0 @@
-interface Task{
-  id: string;
-  title: string;
-  description: string;
-  selectedDate: string;
-  isChecked: boolean;
-  isEdit: boolean;
-}
-export default Task;
